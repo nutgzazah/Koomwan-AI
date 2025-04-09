c361edff733caa261d8f7a822cff992f7ef01e36
JaTingJA <55779319+nutgzazah@users.noreply.github.com>
2025-04-09 20:46:25 +0700

    update port server

--- app.py
+++ app.py
@@ -46,4 +46,6 @@ def predict():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # ดึง PORT จาก environment variable
+    app.run(host="0.0.0.0", port=port, debug=True)
